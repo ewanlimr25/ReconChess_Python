7c73b68961bfff3e2908632b1126fa3edc3bec23
Ewan Li <liyuxuan66@hotmail.com>
2021-06-13 22:01:05 -0400

    Adding Utility folder to hold common scripts

---
+++ Util/Common_Util.py
@@ -0,0 +1,4 @@
+from reconchess import *
+
+## The purpose of this file is to establit a common functionality to be used to calculate movesets, weights, etc.
+## This is a work in process for common functions. Specific calcualtion files will be separate from this one.
