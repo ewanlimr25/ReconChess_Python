21307b88244b4f348ad6f4e06fbe97a40e6afa37
Prashanth <psvelayu@gmail.com>
2021-08-01 23:15:33 -0400

    Added 4 system moves for ConservitiveBot.py and changed stockfish path on TroutBot.py

--- Bots/ConservitiveBot.py
+++ Bots/ConservitiveBot.py
@@ -1,11 +1,55 @@
+import chess.engine
 import random
+import os
 from reconchess import *
+
+STOCKFISH_ENV_VAR = '../stockfish/stockfish_13_win_x64_bmi2.exe'
+
+WHITE_SYSTEM = [chess.Move.from_uci('g1f3'), 
+    chess.Move.from_uci('g2g3'),
+    chess.Move.from_uci('f1f2'),
+    chess.Move.from_uci('e1g1')]
+
+BLACK_SYSTEM = [chess.Move.from_uci('g8f6'), 
+    chess.Move.from_uci('g7g6'),
+    chess.Move.from_uci('f8f7'),
+    chess.Move.from_uci('e8g8')]
 
 ## Will construct this bot to be conservative and play within own 4 spaces and establish defences.
 
-class RandomBot(Player):
+class conservative_bot(Player):
+    def __init__(self):
+        ## set the board as global to understand board state, can be used in choose_sense, choose_move
+        self.myBoard = None
+
+        ## set the color for our player
+        self.color = None
+
+        ## set the place where you piece was captured.
+        self.my_piece_captured_square = None
+
+        ## set a global for turn count and calculate possible moves.    
+        self.myTurn = 0
+
+        ## set to remember where my pieces are. This is a Dictionary that contains key:value where is chess.Piece : chess.Square
+        self.myPieces = None
+
+        ## set to determine which set of opening moves will be used
+        self.my_system = None
+
+        ## set to keep track of how many moves of hard-coded theory have been made.
+        ## starts at -1 as the value is incremented prior to choose_move returning the next theory move.
+        self.opening_count = -1
+
+
     def handle_game_start(self, color: Color, board: chess.Board, opponent_name: str):
-        pass
+        self.board = board
+        self.color = color  # Boolean value where chess.WHITE = True and chess.BLACK = FALSE
+        if self.color:
+            self.my_system = WHITE_SYSTEM
+        else:
+            self.my_system = BLACK_SYSTEM
+
 
     def handle_opponent_move_result(self, captured_my_piece: bool, capture_square: Optional[Square]):
         pass
@@ -18,9 +62,14 @@ class RandomBot(Player):
         pass
 
     def choose_move(self, move_actions: List[chess.Move], seconds_left: float) -> Optional[chess.Move]:
-        try: 
-            return 
-        except:    
+        if self.my_system[self.opening_count] in move_actions:
+            self.opening_count += 1
+            print(self.opening_count)
+            print("My theory move is {}".f(self.my_system[self.opening_count]))
+            return WHITE_SYSTEM[self.opening_count]
+        else:
+            print(self.opening_count)
+            print("Unfortunately, the move {} was not in the list: \n{}".format(self.my_system[self.opening_count], move_actions))
             return random.choice(move_actions + [None])
 
     def handle_move_result(self, requested_move: Optional[chess.Move], taken_move: Optional[chess.Move],
