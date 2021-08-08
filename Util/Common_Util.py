4201165c8c4aae9602c1ef421ffe54642df2580c
Ewan Li <liyuxuan66@hotmail.com>
2021-08-07 23:34:19 -0400

    Structured choose_sense in LearningBot.py and added utility module generatePieces(color) in Common_Util.py

--- Util/Common_Util.py
+++ Util/Common_Util.py
@@ -8,3 +8,24 @@ class CommonUtility:
 
     def generateBoard(self) -> chess.BaseBoard:
         return chess.BaseBoard
+
+
+    def generatePieces(self, color):
+        pieces = dict()
+        if color == chess.WHITE:
+            pieces["Rook"] = [chess.A1, chess.H1]
+            pieces["Knight"] = [chess.B1, chess.G1]
+            pieces["Bishop"] = [chess.C1, chess.F1]
+            pieces["Queen"] = [chess.D1]
+            pieces["King"] = [chess.E1]
+            pieces["Pawn"] = [chess.A2, chess.B2,chess.C2,chess.D2,chess.E2,chess.F2,chess.G2,chess.H2]
+        else:
+            pieces["Rook"] = [chess.A8, chess.H8]
+            pieces["Knight"] = [chess.B8, chess.G8]
+            pieces["Bishop"] = [chess.C8, chess.F8]
+            pieces["Queen"] = [chess.D8]
+            pieces["King"] = [chess.E8]
+            pieces["Pawn"] = [chess.A7, chess.B7,chess.C7,chess.D7,chess.E7,chess.F7,chess.G7,chess.H7]
+        return pieces
+    
+
