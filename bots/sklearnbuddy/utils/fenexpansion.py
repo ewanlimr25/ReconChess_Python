96e9a2a8ddd100256111df0693b2933a1990fa98
Prashanth Velayudhan <psvelayu@gmail.com>
2022-07-05 22:37:56 -0400

    improve documentation and add non-piece fen information to board data

--- bots/sklearnbuddy/utils/fenexpansion.py
+++ bots/sklearnbuddy/utils/fenexpansion.py
@@ -15,19 +15,29 @@ import pandas as pd
     6. fullmove number
 """
 
+# Initialize a blank board that can capture any FEN in dummy variable form
 def blankboard():
     board_coords = []
+    
+    # 1. pieces
     for number in ['1', '2', '3', '4', '5', '6', '7', '8']:
         for letter in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']:
             for piecetype in ['P', 'N', 'B', 'R', 'Q', 'K', 'p', 'n', 'b', 'r', 'q', 'k', '@']:
                 board_coords.append(letter + number + piecetype)
+    
+    # 2. active colour 
+    board_coords.append("active_colour")
+
+    # 3. castling
+    board_coords.append("K_castle_rights") # white kingside castle
+    board_coords.append("Q_castle_rights") # white queenside castle
+    board_coords.append("k_castle_rights") # black kingside castle
+    board_coords.append("q_castle_rights") # black queenside castle
+
+    # 4. 
     board_df = pd.DataFrame(columns = board_coords)
     return(board_df)
 
-def miniboard(): # practice with a smaller board before implementing blankboard
-    board_coords = []
-    for number in ['1', '2', '3']:
-        pass
 
 def fenexpansion(board):
     # Split the FEN string into individual fields
@@ -40,13 +50,13 @@ def fenexpansion(board):
     en_passant = split_fen_full[3]
     halfmove_clock = split_fen_full[4]
     fullmove_number = split_fen_full[5]
-
+    
+#   1. HANDLE PIECE INFORMATION ===========================================
     # Remove the slashes (chess board rank separators) from the string
     pieces = pieces.replace("/","")
     expanded_board = ''
     
-    # Iterate through pieces and replace blank numbers with the appropriate
-    # number of '@'s. 
+    # Replace blank squares with the appropriate number of '@'s. 
     for row in pieces:
         for character in row:
             if character.isdigit():
@@ -55,9 +65,6 @@ def fenexpansion(board):
                 expanded_board += character
 
     # Initialize blankboard with all 832 columns
-    # Each column is a unique piece-square combination (e.g., a1Q is a column
-    # with the value of '1' when there is a white queen on a1 and '0'
-    # otherwise.
     board_data = blankboard()
     
     # List of all the column names in the main 832 column dataframe
@@ -66,23 +73,20 @@ def fenexpansion(board):
     # add a row of all zeros to the board
     board_data.loc[len(board_data)] = 0
 
-    # determine which position on the board (e.g., a4) each piece belongs to
-    # then flip that piece from a 0 to a 1
+    # If a piece is on a square, assign a '1' to the appropriate column
     for i in range(len(expanded_board)): 
         piece = expanded_board[i]
         rank = i // 8 + 1
         file = "abcdefgh"[i % 8]
         present_piece = file + str(rank) + piece
         board_data.loc[len(board_data)-1, present_piece] = 1
+
+
     
-    print(board_data)
     board_data.to_csv("board_data.csv")
-    
-    board_data.iloc[0, 1] = 1
-    a = "a1P"
-    board_data.loc[0, a] = 1
+    return(board_data)
 
 
 board = "r1b1kbnr/ppp1p1pp/n7/3P1P1Q/3PP3/1BN2P2/PP2N1PP/R1BQK2R b KQkq - 0 5"
 
-fenexpansion(board)
+print(fenexpansion(board))
