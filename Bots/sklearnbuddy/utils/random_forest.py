1a4001ef60b30841539a8de7aead85dd5231fb72
Prashanth Velayudhan <psvelayu@gmail.com>
2022-11-08 22:59:07 -0500

    eda

--- Bots/sklearnbuddy/utils/random_forest.py
+++ Bots/sklearnbuddy/utils/random_forest.py
@@ -5,9 +5,45 @@ import sklearn.linear_model
 import os
 from plotnine import ggplot, geom_point, aes
 
+# Import game data
 games = pd.read_csv("first_50_games.csv")
 
-print(games)
+# Create new variable indicating if the requested move was played by winner
+games["winner_move"] = (games["white_won"] == games["whites_turn"])
+
+# Just examine the winner move-related columns
+games.loc[:, "white_won":"winner_move"]
+
+
+games["requested_move"].unique().size
+
+# white's losers
+whites_losers = games[(games["winner_move"] == 0) &
+                      (games["whites_turn"] == 1)]
+
+# black's losers
+blacks_losers = games[(games["winner_move"] == 0) &
+                      (games["whites_turn"] == 0)]
+
+# white's winners
+whites_winners = games[(games["winner_move"] == 1) &
+                       (games["whites_turn"] == 1)]
+
+# black's winners
+blacks_winners = games[(games["winner_move"] == 1) &
+                       (games["whites_turn"] == 0)]
+
+whites_losers_counts = whites_losers['requested_move'].value_counts()
+whites_winners_counts = whites_winners['requested_move'].value_counts()
+blacks_losers_counts = blacks_losers['requested_move'].value_counts()
+blacks_winners_counts = games['requested_move'].value_counts()
+
+
+print(count)
+
+
+count = df['A'].value_counts()
+print(count)
 
 print(type(games))
 
@@ -17,8 +53,6 @@ y = np.c_[games["requested_move"]]
 games.plot(kind='scatter', x='a1P', y='requested_move')
 plt.show()
 
-(ggplot(games, aes(x='a1P', y='requested_move'))
- + geom_point()
  
 
 from plotnine import ggplot, geom_point, aes, stat_smooth, facet_wrap
